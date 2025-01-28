7c3f2fd906dfebe66f5a3c8d7abf07906e30ca9c
gastonmiguel <gastonmiguel@live.com.ar>
2025-01-28 19:12:36 -0300

    add manifest

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,22 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import Head from 'next/head';
+import { Metadata } from 'next';
 
 export const experimental_ppr = true;
 
+export const metadata: Metadata = {
+  title: "ClubMio",
+  description: "Aplicación de gestión de clubes de barrio.",
+  manifest: "/web.manifest"
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
+  
   return (
-    <>
-      <Head>
-        <link rel="manifest" href="/dashboard/manifest.json" />
-      </Head>
-      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
         <div className="w-full flex-none md:w-64">
           <SideNav />
         </div>
         <div className="flex-grow py-2 px-4 md:overflow-y-auto md:p-12">{children}</div>
-      </div>
-    </>
+    </div>
   );
 }
